6863d8651afe1d61aff57ab8608ea0ffe2a3ff32
yvan <chavez.yvan@gmail.com>
2020-05-30 09:07:26 -0500

    Subiendo empaquetado de AgendaApp

---
+++ precache-manifest.bd2d1b8ed27625ae3059a0cc4ef5e425.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0cee140045bb15a4197dc517fd6631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af858a5165bd28fafd1",
+    "url": "/static/js/2.1a4cdf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a4cdf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f503c98166b4af53dadf",
+    "url": "/static/js/main.3ad34412.chunk.js"
+  },
+  {
+    "revision": "e275a57e96d358f64ebf",
+    "url": "/static/js/runtime-main.6dfc73c8.js"
+  }
+]);
